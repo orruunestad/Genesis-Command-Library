c26dfc3a6475d5cfa677b7c144dba1e213b313fa
orruunestad <runestadowen@gmail.com>
2025-02-22 23:06:13 -0600

    Create source.lua

---
+++ source.lua
@@ -0,0 +1,23 @@
+local Players = game:GetService("Players")
+local LocalPlayer = Players.LocalPlayer
+local Commands = {}
+local Prefix = "!" -- set with setPrefix("new prefix goes here")
+
+function setPrefix(newPrefix)
+    Prefix = newPrefix or "!"
+end
+
+function addCommand(trigger, action)
+    Commands[trigger:lower()] = action
+end
+
+LocalPlayer.Chatted:Connect(function(message)
+    if message:sub(1, #Prefix) == Prefix then
+        local args = message:sub(#Prefix + 1):split(" ")
+        local cmd = args[1]:lower()
+        if Commands[cmd] then
+            table.remove(args, 1)
+            Commands[cmd](unpack(args))
+        end
+    end
+end)
